ed5fb5cdab27c38a0730f624cc01f20dd367e8a4
Fayzulin694 <abdujalilovfayzullo0407@gmail.com>
2024-02-21 23:41:54 +0500

    typescript lesson-1

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,84 @@
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
+
+// styles
+import styles from './TaskForm.module.css';
+
+// interface
+import { ITask } from '../interfaces/Task'
+
+interface Props {
+  btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+  task?: ITask | null;
+  handleUpdate?(id: number, title: string, difficulty: number): void;
+}
+
+const TaskForm = ({btnText, taskList, setTaskList, task, handleUpdate}: Props) => {
+
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
+
+  useEffect(() => {
+    if(task) {
+      setId(task.id);
+      setTitle(task.title);
+      setDifficulty(task.difficulty);
+    }
+  }, [task])
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(handleUpdate) {
+      handleUpdate(id, title, difficulty);
+    }else {
+      const id = Math.floor(Math.random() * 1000);
+
+      const newTask: ITask = {id, title, difficulty};
+
+      setTaskList!([...taskList, newTask]);
+
+      setTitle("");
+
+      setDifficulty(0);
+    }
+
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.name === "title") {
+      setTitle(e.target.value);
+    }else {
+      setDifficulty(parseInt(e.target.value));
+    }
+  };
+
+  return (
+    <form onSubmit={addTaskHandler} className={styles.form}>
+      <div className={styles.input_container}>
+        <label htmlFor="title">Text</label>
+          <input 
+            type="text" 
+            name='title' 
+            placeholder='Text' 
+            onChange={handleChange} 
+            value={title} 
+          />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="dificulty">Number</label>
+        <input 
+          type="text" 
+          name='dificulty' 
+          placeholder='Number' 
+          onChange={handleChange} 
+        />
+      </div>
+      <input type="submit"/>
+    </form>
+  )
+}
+
+export default TaskForm
